6b96295ea01ee80e6dcaa75db4058435bc4f238f
bytedance <baihaihui@bytedance.com>
2021-12-28 20:53:39 +0800

    feat: 业务模型

--- lib/model/basic_habit.dart
+++ lib/model/basic_habit.dart
@@ -5,6 +5,11 @@ enum HabitType {
   WithNumber, // 是否有数值在打卡的时候必填
 }
 
+enum CheckFrequency {
+  Week,
+  Month,
+}
+
 // 习惯，创建新的习惯的时候需要填的信息
 class BasicHabit {
   late String name; // 习惯名称
@@ -12,4 +17,5 @@ class BasicHabit {
   late HabitIcon habit_icon; // 习惯icon
   late String desc; // 习惯说明，可以用来展示，是否需要长度限制？
   HabitType type = HabitType.Basic; // 习惯类型
+
 }
