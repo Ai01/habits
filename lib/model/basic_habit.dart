e4a1949559a2c7b11a79843f1ebc374e14ec1c62
bytedance <baihaihui@bytedance.com>
2021-12-28 21:07:32 +0800

    feat: 业务模型

--- lib/model/basic_habit.dart
+++ lib/model/basic_habit.dart
@@ -6,8 +6,20 @@ enum HabitType {
 }
 
 enum CheckFrequency {
-  Week,
-  Month,
+  Day, // 每日
+  Week, // 按周
+  Month, // 按月
+  Year, // 按年
+}
+
+enum WeekDay {
+  Monday,
+  Tuesday,
+  Wednesday,
+  ThursDay,
+  Friday,
+  Saturday,
+  Sunday,
 }
 
 // 习惯，创建新的习惯的时候需要填的信息
@@ -15,7 +27,15 @@ class BasicHabit {
   late String name; // 习惯名称
   late String id; // 习惯id
   late HabitIcon habit_icon; // 习惯icon
-  late String desc; // 习惯说明，可以用来展示，是否需要长度限制？
+  late String desc; // 习惯说明，可以用来展示
   HabitType type = HabitType.Basic; // 习惯类型
 
+  // 如果是frequency是Day，count是1。那么代表每日一次。
+  // 如果是frequency是周，count是2, 那么周一到周日都展示出来，直到用户打卡完成
+  CheckFrequency frequency = CheckFrequency.Day; // 打卡频率
+  int count = 1; // 打卡次数
+
+  late int should_check_count; // 应该打卡次数
+  late int check_count; // 实际打卡次数
+  late bool is_finished; // 是否完成
 }
